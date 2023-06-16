e6e31b9144e2ca0ad5c7b743ff842b4b98c96616
Alexandre-enseirb <Alexandre.vd@laposte.net>
2023-06-16 11:51:02 +0200

    Update to params
    Separated Interpolation factor and decimation factor
    Introduced Oversampling at Rx
    Replaced strcmp with strcmpi (case insensitive comparison)

--- Radios/getRadioParams.m
+++ Radios/getRadioParams.m
@@ -1,21 +1,26 @@
 function [params] = getRadioParams(radioType)
 
 params = struct();
-params.centerFrequency = 500e6;
-params.LOOffset = 0;
-params.enableBurstMode = false;
-params.nFramesInBurstMode = 100;
-params.dataType = 'int16';
-params.PPSSource = 'internal';
-params.ClockSource = 'external';
-params.MasterClockRate = 30e6;
+params.centerFrequency = 500e6;   % Frequence porteuse
+params.LOOffset = 0;              % Offset de l'oscillateur local
+params.enableBurstMode = false;   % Mode 'rafale'
+params.nFramesInBurstMode = 100;  % Nombre de frames par rafale
+params.dataType = 'int16';        % Type de donnees
+params.PPSSource = 'internal';    % Source de pulsations
+params.ClockSource = 'external';  % Source d'horloge
+params.MasterClockRate = 30e6;    % Rythme de travail de la radio
+params.InterpolationFactor = 30;  % Facteur d'interpolation de l'emetteur
+params.DecimationFactor = 30;     % Facteur de decimation du recepteur
+params.RxOversampling = params.InterpolationFactor/params.DecimationFactor; % Surechantillonnage du recepteur
 
-if strcmp(radioType, 'tx') || strcmp(radioType, 'Tx')
+if strcmpi(radioType, 'tx')
+    % Parametres specifiques a l'emetteur
     params.Gain = 40;
     params.ChannelMapping = [1 2];
-    params.InterpolationDecimationFactor = 30;
-elseif strcmp(radioType, 'rx') || strcmp(radioType, 'Rx')
+    params.InterpolationDecimationFactor = params.InterpolationFactor; % pour la retrocompatibilite du code
+elseif strcmpi(radioType, 'rx')
+    % Parametres specifiques au recepteur
     params.Gain = 35;
     params.ChannelMapping = 1;
-    params.InterpolationDecimationFactor = 30;
+    params.InterpolationDecimationFactor = params.DecimationFactor; % pour la retrocompatibilite du code
 end
