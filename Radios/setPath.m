1de972905bdb62ca5b24108fc61375b6c663b76b
Alexandre-enseirb <Alexandre.vd@laposte.net>
2023-07-04 16:49:48 +0200

    chargement des librairies dans le path

--- Radios/setPath.m
+++ Radios/setPath.m
@@ -1,5 +1,14 @@
 function setPath()
-
-    addpath("functions", "Data");
+    
+    addpath("functions", ...    % vrac de fonctions douteuses
+        "Data", ...             % Donnees exploitables (images, fichiers .mat)
+        "../libs/libwalsh", ... % Fonctions relatives a la transformee de Walsh
+        "../libs/libcom", ...   % Fonctions relatives aux com. num.
+        "../libs/libsig", ...   % Fonctions relatives au traitement du signal
+        "../libs/libhilbert", ... % Fonctions relatives au filtrage de Hilbert
+        "../libs/libpseudosemantic/", ... % fonctions relatives au mapping OSDM/dictionnaire
+        "../libs/libplot", ... % fonctions relatives a l'affichage
+        "../libs/libsim", ...  % Fonctions pour lancer des simulations
+        "../libs/generators"); % Generateurs de structures (~factories)
 
 end
