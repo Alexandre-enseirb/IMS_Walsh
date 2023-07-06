48c0ed077dc08460fb7689c254e107ad24321da1
Alexandre-enseirb <Alexandre.vd@laposte.net>
2023-07-06 09:51:05 +0200

    Tests Windows

--- Radios/rxLauncher.bat
+++ Radios/rxLauncher.bat
@@ -1,6 +1,12 @@
 @echo off
+REM Remplacer cette variable par le dossier d'installation de MATLAB
 set matlab=C:\Program Files\MATLAB\R2023a\bin\matlab.exe
 
-start /B matlab -nodesktop -nosplash -batch "parallel_receiver; exit;" > rx_logs.txt
+REM Dans la variable `programName`, mettre le nom du script Matlab a lancer
+REM (ex : `parallel_receiver`) SANS l'extension de fichier.
+REM Sinon, MATLAB va etre perdu et lancer une erreur.
+set programeName = "parallel_receiver"
+
+start /B matlab -nodesktop -nosplash -batch "%programName%; exit;" > rx_logs.txt
 
 pause
--- Radios/txLauncher.bat
+++ Radios/txLauncher.bat
@@ -1,6 +1,12 @@
 @echo off
+REM Remplacer cette variable par le dossier d'installation de MATLAB
 set matlab=C:\Program Files\MATLAB\R2023a\bin\matlab.exe
 
-start /B matlab -nodesktop -nosplash -batch "parallel_transmitter; exit;" > tx_logs.txt
+REM Dans la variable `programName`, mettre le nom du script Matlab a lancer
+REM (ex : `parallel_transmitter`) SANS l'extension de fichier.
+REM Sinon, MATLAB va etre perdu et lancer une erreur.
+set programeName = "parallel_transmitter"
+
+start /B matlab -nodesktop -nosplash -batch "%programeName%; exit;" > tx_logs.txt
 
 pause
