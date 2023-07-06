7f7586fa13dcf6a81b8b494e62f7d1f4404c8ac0
Alexandre-enseirb <71506491+Alexandre-enseirb@users.noreply.github.com>
2023-07-06 08:02:28 +0200

    Scripts BATCH testés sous Windows
    Ca fonctionne

--- Radios/rxLauncher.bat
+++ Radios/rxLauncher.bat
@@ -5,8 +5,15 @@ set matlab=C:\Program Files\MATLAB\R2023a\bin\matlab.exe
 REM Dans la variable `programName`, mettre le nom du script Matlab a lancer
 REM (ex : `parallel_receiver`) SANS l'extension de fichier.
 REM Sinon, MATLAB va etre perdu et lancer une erreur.
-set programeName = "parallel_receiver"
 
+REM Ne pas mettre de guillemets autour du nom du fichier non plus
+set programName=parallel_receiver
+
+REM lance MATLAB sans affichages pour economiser des ressources et utiliser au
+REM mieux les radios
 start /B matlab -nodesktop -nosplash -batch "%programName%; exit;" > rx_logs.txt
 
+REM met l'invite de commande en pause tant qu'aucune action n'est realisee cote utilisateur
+REM pas forcement utile actuellement mais interessant si on veut ecrire dans l'invite de commande
+REM puis consulter les messages ecrits
 pause
--- Radios/txLauncher.bat
+++ Radios/txLauncher.bat
@@ -2,11 +2,19 @@
 REM Remplacer cette variable par le dossier d'installation de MATLAB
 set matlab=C:\Program Files\MATLAB\R2023a\bin\matlab.exe
 
+
 REM Dans la variable `programName`, mettre le nom du script Matlab a lancer
 REM (ex : `parallel_transmitter`) SANS l'extension de fichier.
 REM Sinon, MATLAB va etre perdu et lancer une erreur.
-set programeName = "parallel_transmitter"
 
-start /B matlab -nodesktop -nosplash -batch "%programeName%; exit;" > tx_logs.txt
+REM Ne pas mettre de guillemets autour du nom du fichier non plus
+set programName=img_transmitter
 
+REM lance MATLAB sans affichages pour economiser des ressources et utiliser au
+REM mieux les radios
+start /B matlab -nodesktop -nosplash -batch "%programName%; exit;" > tx_logs.txt
+
+REM met l'invite de commande en pause tant qu'aucune action n'est realisee cote utilisateur
+REM pas forcement utile actuellement mais interessant si on veut ecrire dans l'invite de commande
+REM puis consulter les messages ecrits
 pause
