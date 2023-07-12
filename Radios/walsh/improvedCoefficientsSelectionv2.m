3c3b04abb1711ed1b1d6b805ae5dea63c659a19e
Alexandre-enseirb <Alexandre.vd@laposte.net>
2023-07-12 15:13:28 +0200

    Bibliotheque Walsh supplementaire pour les radios

---
+++ Radios/walsh/improvedCoefficientsSelectionv2.m
@@ -0,0 +1,152 @@
+function [combinations] = improvedCoefficientsSelectionv2(nCoefficientsToSelect, ...
+    cluster, ...
+    carrier, ...
+    nSymbolsCombinationsPerCoefficientsCombinations, ...
+    nModSymbPerOSDMSymb, ...
+    nSymbOSDMTx, ...
+    nCombinationsToGenerate, ...
+    symbOSDMDuration, ...
+    coeffCarrier, ...
+    attenuationFactor, ...
+    stopAtMax, ...
+    params)
+%IMPROVEDCOEFFICIENTSSELECTIONV2 generates combination of modulated Walsh coefficients
+%
+%   [combinations] = IMPROVEDCOEFFICIENTSSELECTIONV2(nCoefficientsToSelect, cluster, nSymbolsCombinationsPerCoefficientsCombinations,
+%   nCombinationsToGenerate, params) generates combinations of modulated Walsh coefficients and tests them with the given modulation.
+
+% dictionnary to store the ouput
+combinations = dictionary();
+
+cluster = fliplr(cluster);
+
+depth = nCoefficientsToSelect;
+nBestFitsPerDepth = 4;
+
+% fft of every cluster 2 function
+fftWalsh = zeros(length(cluster), params.Nfft);
+
+for iFFT = 1:length(cluster)
+    fftWalsh(iFFT, :) = abs(fftshift(fft(params.W(cluster(iFFT, :), :), params.Nfft))).^2;
+    % fftWalsh(iFFT, :) = 20*log10(fftWalsh(iFFT,:)/max(fftWalsh(iFFT,:)));
+end
+
+% avoid infinite values
+% fftWalsh(fftWalsh==-Inf) = 0;
+
+coefficients = findBestFits(cluster, fftWalsh, nBestFitsPerDepth, depth, params);
+iCoefficients = 1;
+validCombinations = 0;
+
+% Generation of valid combinations
+while validCombinations < nCombinationsToGenerate || ~stopAtMax
+    % Test a certain amount of combinations per coefficients family
+    for iReload=1:nSymbolsCombinationsPerCoefficientsCombinations
+        % Since we have no clue for now about which coefficients are best, we randomize it
+        [~, sigdB, ~, realVals] = createRealSignal(nModSymbPerOSDMSymb, nSymbOSDMTx, 0, carrier, symbOSDMDuration, coefficients(iCoefficients, :), coeffCarrier, attenuationFactor, params);
+        % If the generated signal has a conform spectrum
+        if isConform(sigdB(params.nullFrequencyIdx:params.maxConformFrequency), params.BW_visible(params.nullFrequencyIdx:params.maxConformFrequency).')
+            % Save it to the dictionary.
+            [coeffsSorted, idxPerm] = sort(coefficients(iCoefficients, :), "ascend");
+            realVals = realVals(idxPerm);
+            savedData = [coeffsSorted; realVals.'];
+            key = keyHash(savedData);
+            % Evite les doublons
+            if combinations.numEntries == 0 || ~combinations.isKey(key)
+                combinations(key) = {savedData};
+                validCombinations = validCombinations + 1;
+            end
+        end
+        
+    end
+    iCoefficients = iCoefficients+1;
+    if iCoefficients > size(coefficients, 1)
+        fprintf(2, "Could not generate enough combinations. Exiting early.");
+        validCombinations = nCombinationsToGenerate + 1; % exit the while in a clean way
+        stopAtMax = true; % ensures we leave even if we wanted combinations for every coefficient families
+    end
+    fprintf("[%d]: %d/%d done\n",iCoefficients, validCombinations, nCombinationsToGenerate);
+end
+end
+
+function toto = tata(tutu)
+
+foo = bar;
+
+end
+
+
+
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+function [coefficients] = findBestFitsRecursive(coefficients, cluster2, powerSpectrums, nBestFitsPerDepth, depth, maxDepth, params)
+
+% Determination du nombre de repetitions a cette profondeur
+nRepeat = nBestFitsPerDepth^(maxDepth-depth);
+nCoeffs = size(coefficients, 1);
+
+% Determination des coefficients de la profondeur donnee
+[~, idxIn] = ismember(coefficients(:, 1:depth-1), cluster2);
+% Par construction de la recursion, on a une seule valeur dans idxIn, donc
+idxIn = unique(idxIn);
+[~, idxOut] = setdiff(cluster2, cluster2(idxIn));
+
+% Creation du signal correspondant a nos indices d'entree
+sigIn = zeros(size(params.W(1,:)));
+
+% On accumule nos signaux temporels
+for i=1:length(idxIn)
+    sigIn = sigIn + params.W(idxIn(i), :);
+end
+
+% FFT et on duplique autant de fois que l'on veut tester de combinaisons
+powerSpectrumsIn = abs(fftshift(fft(sigIn, params.Nfft))).^2;
+powerSpectrumsIn = repmat(powerSpectrumsIn, length(idxOut), 1);
+
+% On calcule enfin nos combinaisons et on va trier celles qui ont le meilleur etalement
+correlation = correlationNoLag(powerSpectrumsIn, powerSpectrums(idxOut, :));
+
+[~, idx] = sort(correlation, 'ascend');
+toAdd = repmat(cluster2(idxOut(idx(1:nBestFitsPerDepth))).', nRepeat, 1);
+coefficients(:, depth) = toAdd(:);
+if depth < maxDepth
+    for i=1:(nCoeffs/nRepeat)
+        idxStart = (i-1) * nRepeat + 1;
+        idxStop = i*nRepeat;
+        coefficients(idxStart:idxStop, :) = findBestFitsRecursive(coefficients(idxStart:idxStop, :), cluster2, powerSpectrums, nBestFitsPerDepth, depth+1, maxDepth, params);
+    end
+end
+end
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+function [coefficients] = findBestFits(cluster2, powerSpectrums, nBestFitsPerDepth, maxDepth, params)
+
+nRepeat = nBestFitsPerDepth^(maxDepth-1);
+nRows = nRepeat * length(cluster2);
+
+firstCol = repmat(cluster2.', nRepeat, 1);
+firstCol = firstCol(:);
+
+coefficients = zeros(nRows, maxDepth);
+coefficients(:, 1) = firstCol;
+
+% Initialisation de la recusion
+depth = 1;
+
+% Parcours de tous les elements du cluster pour lancer la recursion
+for iCluster = 1:length(cluster2)
+    idxStart = (iCluster-1) * nRepeat + 1;
+    idxStop  = iCluster * nRepeat;
+    coefficients(idxStart:idxStop, :) = findBestFitsRecursive(coefficients(idxStart:idxStop, :), cluster2, powerSpectrums, nBestFitsPerDepth, depth+1, maxDepth, params);
+end
+end
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+function [correlation] = correlationNoLag(sigToAnalyze, otherSigs)
+
+correlation = sum(sigToAnalyze.*otherSigs, 2);
+
+end
---
+++ Radios/walsh/wse.m
@@ -0,0 +1,61 @@
+function [sig_Walsh, Xw_b] = wse(sig_in, params, N, truncature)
+%WSE est la decomposition en series de Walsh d'un signal
+%
+%   [SIG_WALSH] = WSE(SIG_IN, PARAMS, N) calcule les coefficients de Walsh
+%   associes a un signal SIG_IN et retourne sa decomposition en series de
+%   Walsh. Les parametres PARAMS.OSR et PARAMS.ORDER sont utilises pour
+%   calculer le surechantillonnage du signal et l'ordre de la decomposition
+%   de Walsh. N est la duree du signal produit et correspond au plus petit
+%   commun multiple de la duree d'une periode de rafraichissement et de la
+%   periode associee a la plus haute frequence du signal.
+%
+%   [SIG_WALSH, XW_B] = WSE(SIG_IN, PARAMS, N) calcule les coefficients de
+%   Walsh associes a un signal SIG_IN et retourne sa decomposition en
+%   series de Walsh. Les parametres PARAMS.OSR et PARAMS.ORDER sont
+%   utilises pour calculer le surechantillonnage du signal et l'ordre de 
+%   la decomposition de Walsh. XW_B contient les 2^PARAMS.ORDER
+%   coefficients de Walsh utilises pour la reconstruction.
+
+if ~exist("truncature", "var") || (isa(truncature,"logical") && truncature == false)
+    truncature = false;
+elseif ~isa(truncature,"double") || int64(truncature) ~= truncature
+    error("Please specify an integer for the truncature");
+end
+
+Xw = dwt(sig_in(1:params.osr:end), params.W, params.order, true);
+[~, nWindows] = size(Xw);
+
+sig_Walsh = zeros(1, N);
+
+Xw_b = quantification(Xw, params.nBitsAmp, params.maxBin);
+% Xw_b = Xw;
+[len,~] = size(Xw_b);
+mid = len/2;
+if ~isa(truncature,"logical")
+    % tri des valeurs dans l'ordre decroissant, par V.A.
+    [vals, idx] = sort(abs(Xw_b),'descend');
+    % Ajout du signe
+    vals = vals.*sign(Xw_b(idx));
+    
+    % Troncature par colonne
+    for j=1:nWindows
+        % Vecteur de coeffs non nuls sur la colonne
+        Xw_b_nz = vals(vals(:,j)~=0,j);
+        
+        % Vecteur tronque
+        Xw_b_nz = truncate_(Xw_b_nz, truncature);
+
+        % Affectation
+        vals(vals(:,j)~=0,j) = Xw_b_nz;
+        Xw_b(idx(:,j),j) = vals(:,j);
+    end
+end
+%figure();
+for i=1:nWindows
+    sig_Walsh((i-1)*params.nCoeffs+1:i*params.nCoeffs) = params.W*Xw_b(:,i);
+    %plot(params.time_axis(1:i*params.nCoeff), sig_Walsh(1:i*params.nCoeff)); grid on;
+end
+% sig_Walsh = repmat(sig_Walsh(1:nWindows*params.nCoeffs), params.osr, 1);
+% sig_Walsh = sig_Walsh(:);
+end
+
